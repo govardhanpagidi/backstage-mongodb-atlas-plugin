30328f4d52085221ba345a01e4d139a85e55ba53
Govardhan Pagidi <govardhan.pagidi@mongodb.com>
2023-09-16 23:51:11 -0500

    Update ClusterFetchComponent.tsx

--- src/components/ClusterFetchComponent/ClusterFetchComponent.tsx
+++ src/components/ClusterFetchComponent/ClusterFetchComponent.tsx
@@ -18,8 +18,8 @@ import { Link, TableColumn } from '@backstage/core-components';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 const apiUrl = 'http://localhost:8080/api/projects?publicKey=ghewvngy&privateKey=e0702d6b-b062-4a70-bbd0-7044c4f50f75&orgId=63350255419cf25e3d511c95'; // Replace with your API endpoint
-const username = 'ghewvngy';
-const password = 'e0702d6b-b062-4a70-bbd0-7044c4f50f75';
+const username = '';
+const password = '';
 //const apiUrl = 'http://localhost:8081/api/project';
 const registerComponentLink = useRouteRef(registerComponentRouteRef);
 
